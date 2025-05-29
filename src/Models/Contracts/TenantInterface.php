995eaa246de5e878a85a89fbc9585779945abaa1
rtraselbd <rtraselbd72735@gmail.com>
2025-05-29 16:42:19 +0600

    added domains method

--- src/Models/Contracts/TenantInterface.php
+++ src/Models/Contracts/TenantInterface.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Revoltify\Tenantify\Models\Contracts;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 interface TenantInterface
 {
+    public function domains(): HasMany;
+
     public function getTenantKey(): int|string;
 
     public function getTenantKeyName(): string;
