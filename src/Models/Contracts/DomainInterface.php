b5438af44c66b46c45094bfa3c8083bcfeb977e5
rtraselbd <rtraselbd72735@gmail.com>
2025-05-29 16:41:30 +0600

    added more methods

--- src/Models/Contracts/DomainInterface.php
+++ src/Models/Contracts/DomainInterface.php
@@ -9,4 +9,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 interface DomainInterface
 {
     public function tenant(): BelongsTo;
+
+    public function getDomainKey(): int|string;
+
+    public function getDomainKeyName(): string;
+
+    public static function current(): ?static;
+
+    public static function hasCurrent(): bool;
+
+    public function isCurrent(): bool;
 }
